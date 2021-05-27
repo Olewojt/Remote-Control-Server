7b8c640a2ceecdd75bf261284feed80cfcfde20f
wankiet <ketowojec@gmail.com>
2021-05-27 11:29:59 +0200

    Changing directory handling

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from os import pipe
+from os import pipe, getcwd, chdir
 import socket
 import subprocess
 
@@ -7,6 +7,7 @@ HOST = "192.168.1.15"
 PORT = 54545
 FORMAT = "utf-8"
 DISC_MSG = "!dc"
+CWD = getcwd()
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((HOST, PORT))
@@ -28,22 +29,37 @@ def handle_client(conn, addr):
             else:
                 break
         except ConnectionResetError:
-            print(f"[{HOST}] --Nie odpowiada--")
+            print(f"[{HOST}] --Not responding--")
             break        
 
 def command_handle(comm):
     command = comm.lstrip("!").split(" ")
+    global CWD
+    print(CWD)
     try:
-        p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
-        for x in p.stdout:
-            conn.send(x)
+        if command[0]=="cd" and len(command)>=2:
+            tmp = command[1]
+            for x in range(2, len(command), 1):
+                tmp = tmp+" "+command[x]
+            print(tmp)
+            chdir(tmp)
+            CWD = getcwd()     
+        p = subprocess.run(command, shell=True, stdout=subprocess.PIPE, cwd=getcwd())
+        if len(p.stdout)==0 and p.returncode!=0:
+            conn.send(b"Something is wrong.")
+            conn.send(bytes(CWD, FORMAT))
+        elif len(p.stdout)==0 and p.returncode==0:
+            conn.send(b"Command executed.")
+            conn.send(bytes(CWD, FORMAT))
+        else: 
+            conn.send(b"\n"+p.stdout)
+            conn.send(bytes(CWD, FORMAT))
     except:
         #conn.send(bytes(Exception), FORMAT)
-        print("Blad w komendzie.")
-
+        print("Subprocess error. (Most probably)")
+        conn.send(b"Subprocess Error")
 sock.listen()
 print(f"Listening on [{HOST}]")
 while True:
     conn, addr = sock.accept()
-    conn.send(b"Welcome!")
     handle_client(conn, addr)
